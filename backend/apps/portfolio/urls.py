5c908ef4e24f1d065b06102a6e693f978a9cf412
Mohit Khare <mohitkhare582@gmail.com>
2025-10-24 16:18:16 +0530

    Moving to Ruby on Rails

--- backend/apps/portfolio/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PortfolioMeView, PortfolioPublicView
-
-urlpatterns = [
-    path('portfolio/me/', PortfolioMeView.as_view(), name='portfolio-me'),
-    path('portfolio/public/<slug:slug>/', PortfolioPublicView.as_view(), name='portfolio-public'),
-]
