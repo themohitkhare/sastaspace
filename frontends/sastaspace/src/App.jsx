a4265a49657149c810771d244f2ca4838fb060e4
SastaSpace Developer <dev@sastaspace.com>
2026-01-22 16:54:00 +0530

    feat(frontend): create shared components package
    - Extract Navbar, AuthModal, and useAuthStore to frontends/shared/
    - Update sastaspace to import from shared package
    - Enables code reuse across frontend projects

--- frontends/sastaspace/src/App.jsx
+++ frontends/sastaspace/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import Navbar from '@shared/components/Navbar'
 import ProjectCard from './components/ProjectCard'
 
 function App() {
